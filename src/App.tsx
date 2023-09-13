e0966ee5338bf4857b202db1f769ee82f0b54f86
dpchamps <me@davecampion.com>
2023-09-12 21:50:49 -0700

    update description

--- src/App.tsx
+++ src/App.tsx
@@ -19,11 +19,11 @@ function App() {
         <MatchList data={eloData} type={'practice'}/>
         <h2>What is this?</h2>
         <p>
-        This shows the elo scores based on the practice and season 2 matches above. There <a href={'https://docs.google.com/spreadsheets/d/12mc-zRMfN1GbnATjUiZJ_BN8M7V1E2kHiHn0EMHvrCQ/edit#gid=1099556932'}>  pulled from this sheet</a>,
-            which was aggregated by <em>Helios:23.5 Kenneth:5.5</em> on discord (thanks).
+        This shows the elo scores based on the matches listed above. They're <a href={'https://docs.google.com/spreadsheets/d/12mc-zRMfN1GbnATjUiZJ_BN8M7V1E2kHiHn0EMHvrCQ/edit#gid=1099556932'}>  pulled from this sheet</a>,
+        which was mostly aggregated by <em>Helios:23.5 Kenneth:5.5</em> on discord (thanks).
         The more matches, the more "accurate" the number would be.
         </p>
-        <p>It's all just in good fun, anyway. I made it because I was curious. If you've got ideas and time, <a href={'https://github.com/bingo-brawlers-elo/brawlers-elo.github.io'} >send patches</a> ♥</p>
+        <p>It's all just in good fun, and not supposed to be very serious. I made it because I was curious to see what the spread looked like. If you've got ideas and time, <a href={'https://github.com/bingo-brawlers-elo/brawlers-elo.github.io'} >send patches</a> ♥</p>
     </>
   )
 }
